85c04da0d7c5b920cfdf3643e4452b73cdbe6ca2
Lovnish Verma <LovnishVerma@users.noreply.huggingface.co>
2023-12-23 16:50:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import gradio as gr
 from transformers import pipeline
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+# Load pre-trained sentiment analysis model
+classifier = pipeline('sentiment-analysis')
 
-def summarize_text(input_text):
-    summary = summarizer(input_text, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
-    return summary[0]['summary_text']
+# Define the sentiment analysis function
+def analyze_sentiment(text):
+    result = classifier(text)
+    sentiment = result[0]['label']
+    confidence = result[0]['score']
+    return f"Sentiment: {sentiment}, Confidence: {confidence:.4f}"
 
+# Create Gradio interface
 iface = gr.Interface(
-    fn=summarize_text,
+    fn=analyze_sentiment,
     inputs=gr.Textbox(),
     outputs=gr.Textbox(),
     live=True,
-    title="Text Summarization App",
-    description="Enter a longer document, and the model will generate a summary.",
+    title="Sentiment Analysis App",
+    description="Enter a sentence, and the model will predict its sentiment.",
 )
 
+# Launch the Gradio app
 iface.launch()
